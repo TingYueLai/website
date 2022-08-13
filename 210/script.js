5fb9795b451fa12aa0dbb6192bd1b0653bd8bf99
TingYueLai <54653428+TingYueLai@users.noreply.github.com>
2022-08-13 09:20:48 +0800

    Add files via upload

--- 210/script.js
+++ 210/script.js
@@ -1,16 +1,16 @@
 let sendButton = document.getElementById("button");
 var text = document.getElementById("text");
 var date = document.getElementById("date");
+let id= "c_88jdq4kcod9hhsr3o1u67h4j18@group.calendar.google.com";
 function send() {
   alert("已收到填報");
   let name = text.value;
   let phone = date.value;
-  let id= "c_88jdq4kcod9hhsr3o1u67h4j18@group.calendar.google.com;
   document.getElementById("text").value = "";
   document.getElementById("date").value = "";
   //let phone = password.value;
-   $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbxCSIRaHa6SX1MAvCt7N3HzTWMNYpUrRE5gCrnPznLa7bctKckVCddyZz7qW-BFwow3Pw/exec",
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbw1v18cuqzJlx1_R4qGoSa66-_l_voJ6KtpZ92cNGizdUnjTOtwV3fz_le56DuqSDviTw/exec",
     data: {
         "name": name,
         "phone": phone,
@@ -24,3 +24,5 @@ function send() {
   });
 };
 sendButton.addEventListener('click', send);
+
+
